82f012ccccde8571fc69ddd777cd43ba67814d28
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-05-28 21:32:31 +0700

    Template for C++

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned ll
+#define prev HNhu_prev
+#define next HNhu_next
+#define X first
+#define Y second
+#define oo 2000000000
+#define FOR(i,x,y) for(int i=(x);i<=(y);++i)
+#define DWN(i,x,y) for(int i=(x);i>=(y);--i)
+#define ii pair<ll, ll>
+#define umap unordered_map 
+
+using namespace std;
+
+int main () {	
+	ios_base::sync_with_stdio (0);
+}
