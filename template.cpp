04681b1240dd97d78cfc17fe4a3d05b8a8ec26c0
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-06-19 15:09:02 +0700

    New template. Edit for loop

--- template.cpp
+++ template.cpp
@@ -6,13 +6,13 @@
 #define X first
 #define Y second
 #define oo 2000000000
-#define FOR(i,x,y) for(int i=(x);i<=(y);++i)
-#define DWN(i,x,y) for(int i=(x);i>=(y);--i)
+#define FOR(i,x,y) for(int i=(x);i<(y);++i)
+#define DWN(i,x,y) for(int i=(x);i>(y);--i)
 #define ii pair<ll, ll>
-#define umap unordered_map 
-
+#define umap unordered_map // rare
 using namespace std;
 
 int main () {	
 	ios_base::sync_with_stdio (0);
+	
 }
