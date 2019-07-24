5d5031ffe285987818134aafda71059c4320a586
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-07-24 11:10:52 +0700

    Add files via upload

---
+++ template.py
@@ -0,0 +1,30 @@
+# import sys # to have access to sys.stdin
+# from collections import deque # for using deque
+# # deque append, appendleft, pop, popleft
+# from queue import PriorityQueue
+# from bisect import bisect_left as lower_bound, bisect_right as upper_bound
+# array
+def __array (*arg):
+	if len (arg) == 0:
+		return 0
+	return [__array(*arg[1:]) for i in range (arg[0])]
+oo = 2000000000
+
+def main ():
+	
+	pass
+
+if __name__=='__main__':
+	main ()
+'''
+Sorry for leaving the caution here
+
+Template for Competitive Programming with Python 3.x
+Template is under no license, so do not hesitate to use, 
+but I have no intention to take responsibilty of any problems occured
+Uncomment below lines to import (do not encourage to import all, had better be needs)
+PEACE
+
+Contact me via gmail:
+vinhphuctadang@gmail.com
+'''
