d1f2218a12622937e8fa0e5c2ed1f3ed15f73493
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-08-05 11:10:16 +0700

    python-like print (leave uncompleted)

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 #define ll long long
 #define ull unsigned ll
 #define prev HNhu_prev
@@ -11,6 +11,16 @@
 #define ii pair<ll, ll>
 #define umap unordered_map // rare
 using namespace std;
+
+ostream& operator << (ostream& o, std::vector<int>& v) {
+	for (auto it: v)
+		o << it << " ";
+	return o;
+}
+template<typename... Ts> void print (Ts... args){ // inferred from https://en.cppreference.com/w/cpp/language/parameter_pack
+	int t = sizeof...(Ts);
+    int dummy[t] = { (std::cout << args << " ", 0)... };
+}
 
 int main () {	
 	ios_base::sync_with_stdio (0);
