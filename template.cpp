d2c3e1e5309b689e75c46e0d6a2085ecb3338e3f
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-08-06 12:55:04 +0700

    fixed print function

--- template.cpp
+++ template.cpp
@@ -1,28 +1,31 @@
 #include "bits/stdc++.h"
 #define ll long long
 #define ull unsigned ll
-#define prev HNhu_prev
+#define prev HNhu_prev // since default std already has prev, next
 #define next HNhu_next
 #define X first
 #define Y second
 #define oo 2000000000
 #define FOR(i,x,y) for(int i=(x);i<(y);++i)
 #define DWN(i,x,y) for(int i=(x);i>(y);--i)
-#define ii pair<ll, ll>
-#define umap unordered_map // rare
+#define ii pair<ll, ll> // change to <int, int> if need
+#define umap unordered_map // rarely used
 using namespace std;
 
-ostream& operator << (ostream& o, std::vector<int>& v) {
-	for (auto it: v)
-		o << it << " ";
-	return o;
-}
-template<typename... Ts> void print (Ts... args){ // inferred from https://en.cppreference.com/w/cpp/language/parameter_pack
-	int t = sizeof...(Ts);
-    int dummy[t] = { (std::cout << args << " ", 0)... };
-}
+template <typename T>
+ostream& operator << (ostream& o, std::vector<T>& v) {o << "[";FOR(i,0,v.size()) {o << v[i];	if (i < v.size () - 1) o << ", ";}o << "]";return o;}
+template <typename U, typename V>
+ostream& operator << (ostream& o, std::map<U, V>& v) {o << "{"; int __len = v.size (), i = 0;	for (auto& it: v) {i++;	cout << it.first << ":" << it.second; if (i<__len) cout << ", ";} o << "}";	return o;}
+
+// below function inferred from https://en.cppreference.com/w/cpp/language/parameter_pack
+template<typename... Ts> void print (Ts... args){int dummy[sizeof...(Ts)] = { (cout << args, 0)... };}
 
 int main () {	
 	ios_base::sync_with_stdio (0);
-	
+	/* // For test purpose only
+		vector<int> a = {1,2,3,4};
+		vector<string> b = {"TA", "DANG", "VINH", "PHUC"};
+		print (a,'\n',b);
+	*/ 
+	// user code goes here
 }
