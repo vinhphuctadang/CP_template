6cb91ccfbcaa5dc22915220e82ff7efc1e424cfb
vinhphuctadang <30641530+vinhphuctadang@users.noreply.github.com>
2019-08-17 09:23:18 +0700

    Add umap conventional output

--- template.cpp
+++ template.cpp
@@ -15,17 +15,17 @@ using namespace std;
 template <typename T>
 ostream& operator << (ostream& o, std::vector<T>& v) {o << "[";FOR(i,0,v.size()) {o << v[i];	if (i < v.size () - 1) o << ", ";}o << "]";return o;}
 template <typename U, typename V>
-ostream& operator << (ostream& o, std::map<U, V>& v) {o << "{"; int __len = v.size (), i = 0;	for (auto& it: v) {i++;	cout << it.first << ":" << it.second; if (i<__len) cout << ", ";} o << "}";	return o;}
-
+ostream& operator << (ostream& o, std::map<U, V>& v) {o << "{"; int __len = v.size (), i = 0;	for (auto& it: v) {i++;	o << it.first << ":" << it.second; if (i<__len) o << ", ";} o << "}";	return o;}
+template <typename U, typename V>
+ostream& operator << (ostream& o, std::umap<U, V>& v) {o << "{"; int __len = v.size (), i = 0;	for (auto& it: v) {i++;	o << it.first << ":" << it.second; if (i<__len) o << ", ";} o << "}";	return o;}
 // below function inferred from https://en.cppreference.com/w/cpp/language/parameter_pack
 template<typename... Ts> void print (Ts... args){int dummy[sizeof...(Ts)] = { (cout << args, 0)... };}
 
 int main () {	
 	ios_base::sync_with_stdio (0);
-	/* // For test purpose only
-		vector<int> a = {1,2,3,4};
-		vector<string> b = {"TA", "DANG", "VINH", "PHUC"};
-		print (a,'\n',b);
-	*/ 
-	// user code goes here
+	// umap <string, vector<int>> m = {
+	// 	{"hello", {1, 1}},
+	// 	{"world", {2, 3, 4}}
+	// };
+	// print (m);
 }
